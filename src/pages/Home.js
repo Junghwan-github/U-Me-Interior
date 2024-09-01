3e3626b86c6d8faac4a07382520a37747675090e
Park Junhwan <j2studio.jungie2@gmail.com>
2024-09-01 22:41:03 +0900

    main / project start

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Home = () => {
+    return (
+        <div>
+            <h1>U&Me 인테리어 홈페이지가 만들어 지고 있습니다.</h1>
+        </div>
+    )
+}
+
+export default Home;
