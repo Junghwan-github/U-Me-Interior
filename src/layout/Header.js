5926886fa6ce935150929412fda591da01c56681
Park Junhwan <j2studio.jungie2@gmail.com>
2024-09-03 01:22:00 +0900

    feature / test layout

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,12 +1,20 @@
 import React from "react";
-
+import styles from "./Header.module.css";
+import Navigation from "../components/Navigation";
+import GNBNavigation from "../data/GNBNavigation.json";
 
 const Header = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <header className={styles.header}>
+      <nav>
+        <h1 className={styles.logo}>
+          <img src="/images/logo.png" alt="로고" />
+          {/* <span>인테리어</span> */}
+        </h1>
+        <Navigation data={GNBNavigation} />
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
