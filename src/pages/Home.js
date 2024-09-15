b49ff94a02b721e437104a6c90f86703bb235a92
Park Junhwan <j2studio.jungie2@gmail.com>
2024-09-15 16:54:30 +0900

    feature / 섹션 위치 변경 이벤트 섹션 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,17 +8,20 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import ContentItems from "../components/ContentItems";
+import QuickListItem from "../components/QuickListItem";
 
 const Home = () => {
   const ref1 = useRef(null);
   const ref2 = useRef(null);
   const ref3 = useRef(null);
   const ref4 = useRef(null);
+  const ref5 = useRef(null);
 
   const isDisplayOn1 = UseIntersectionObserver(ref1);
   const isDisplayOn2 = UseIntersectionObserver(ref2);
   const isDisplayOn3 = UseIntersectionObserver(ref3);
   const isDisplayOn4 = UseIntersectionObserver(ref4);
+  const isDisplayOn5 = UseIntersectionObserver(ref5);
 
   const settings = {
     dots: false,
@@ -94,6 +97,23 @@ const Home = () => {
           <ContentItems />
         </div>
       </Section>
+      <Section attr={styles.event}>
+        <div
+          ref={ref5}
+          className={isDisplayOn5 ? `${styles.inner} ${styles.on}` : styles.inner}
+        >
+          <ContentTitle
+            title={printTitle(0, 1, "event-title")}
+            desc={printTitle(0, 1, "event-desc")}
+            link={printTitle(0, 1, "event-link")}
+          />
+
+          <div className={styles.event_item}>
+            <div>
+            </div>
+          </div>
+        </div>
+      </Section>
       <Section attr={styles.quick}>
         <div className={styles.inner}>
           <div
@@ -102,36 +122,11 @@ const Home = () => {
               isDisplayOn4 ? `${styles.quick_list} ${styles.on}` : styles.quick_list
             }
           >
-            <div className={styles.quick_item}>
-              <div className={styles.quick_icon}>
-                <img src="/images/wireframe_17472634.png" alt="인테리어 가이드" />
-              </div>
-              <div className={styles.quick_content}>
-                <h5>인테리어 가이드</h5>
-                <p>인테리어 준비 부터 셀프 인테리어 꿀팁 공유!</p>
-              </div>
-            </div>
-            <div className={styles.quick_item}>
-              <div className={styles.quick_icon}>
-              <img src="/images/home-sketch_17456351.png" alt="인테리어 견적문의" />
-              </div>
-              <div className={styles.quick_content}>
-                <h5>시공 견적 문의</h5>
-                <p>부담없이 카카오톡 채팅 문의가 가능해요!</p>
-              </div>
-            </div>
-            <div className={styles.quick_item}>
-              <div className={styles.quick_icon}>
-              <img src="/images/house-moving_17456318.png" alt="인테리어 A/S신청" />
-              </div>
-              <div className={styles.quick_content}>
-                <h5>A/S 신청</h5>
-                <p>최대한 빨리 완벽하게 처리 해드릴께요!</p>
-              </div>
-            </div>
+            <QuickListItem />
           </div>
         </div>
       </Section>
+
     </div>
   );
 };
