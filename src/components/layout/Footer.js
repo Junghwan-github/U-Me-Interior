330a385374b3f9502afe3d02807ba0ccc6b95b77
Park Junhwan <j2studio.jungie2@gmail.com>
2024-09-28 22:56:58 +0900

    feature / 인덱스 레이아웃 1차 완성

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -2,14 +2,54 @@ import React from "react";
 import styles from "./Footer.module.css";
 import FooterSubNavigation from "./FooterSubNavigation";
 
-
 const Footer = () => {
-
   return (
-    <footer className={styles.footer} >
+    <footer className={styles.footer}>
       <FooterSubNavigation />
       <div className={styles.inner}>
-        <p>ⓒ 2024 U&ME 인테리어. </p>
+        <div className={styles.info_wrap}>
+          <dl>
+            <dt>홈페이지 이용안내</dt>
+            <dd>
+              <p>자주 묻는 질문</p>
+              <p>홈페이지 제작 문의</p>
+            </dd>
+            <dd>
+              <span>
+                *현재 홈페이지는 그 어떠한 개인정보를 수집하고 있지 않습니다.
+              </span>
+            </dd>
+          </dl>
+        </div>
+        <div className={styles.corp_wrap}>
+          <dl>
+            <dt>U&ME 인테리어</dt>
+            <dd>
+              <p>대구광역시 달서구 월배로 152 U&ME 인테리어. │ 대표이사 : 장세광</p>
+              <p>연락처 : 010-4434-8729 │ 이메일 : jsk830@naver.com</p>
+              <a href="#">관리자 로그인</a>
+            </dd>
+            <dd className={styles.copy}>
+              <p>
+                ⓒ 2024 <span>U&ME 인테리어.</span> All rights reserved.{" "}
+              </p>
+              <p>
+                Designed By <span>Junghwan.</span>
+              </p>
+            </dd>
+          </dl>
+        </div>
+        <div className={styles.contact_wrap}>
+          <dl>
+            <dt>고객센터 이용안내</dt>
+            <dd>
+              <p className={styles.phone}>전화번호 : 010-4434-8729</p>
+              <p>운영시간 : 09:00 ~ 18:00</p>
+              <p>토요일 : 10:00 ~ 15:00</p>
+              <span>일요일, 공휴일은 휴무입니다.</span>
+            </dd>
+          </dl>
+        </div>
       </div>
     </footer>
   );
--- src/components/ui/BrandAboutItem.js
+++ src/components/ui/BrandAboutItem.js
@@ -2,48 +2,108 @@ import React from "react";
 import styles from "./BrandAboutItem.module.css";
 import { Link } from "react-router-dom";
 import IconRightArrow from "./IconRightArrow";
+import Slider from "react-slick";
 
 const BrandAboutItem = () => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    waitForAnimate: true,
+    autoplay: true,
+    arrows: false,
+    autoplaySpeed: 5000,
+  };
+
   return (
     <div className={styles.content}>
       <div>
         <h2>
-          U&ME 인테리어는<br></br>언제나 고객님의 행복한 공간을<br />최우선으로
-          생각합니다.
+          U&ME 인테리어는<br></br>언제나 고객님의 행복한 공간을
+          <br />
+          최우선으로 생각합니다.
         </h2>
         <p>
           30년 이상의 경력을 가진 각 분야의 전문가들이 함께합니다. <br /> 직접 운영하는
-          공장에서 가구를 자체 제작하여, <br />더욱 높은 품질의 제품을 합리적인 가격에
-          제공합니다.
+          공장에서 가구를 자체 제작하여, <br />
+          더욱 높은 품질의 제품을 합리적인 가격에 제공합니다.
         </p>
         <Link to="/about">
           회사소개 보기 <IconRightArrow direction={"right"} />
         </Link>
       </div>
       <div>
-        <img src="/images/factory.png" alt="공작 사진" />
+        <p className={styles.cover}>
+          공장
+          <br />
+          <span>공장 직접 운영</span>
+        </p>
+        <div>
+          <Slider {...settings}>
+            <div className={styles.factory_item}>
+              <img src="/images/factory.png" alt="공작 사진" />
+            </div>
+            <div className={styles.factory_item}>
+              <img src="/images/factory3.png" alt="공작 사진" />
+            </div>
+          </Slider>
+        </div>
       </div>
       <div>
-        <img src="/images/factory3.png" alt="공작 사진" />
+        <p className={styles.cover}>
+          디자인
+          <br />
+          <span>가구 및 인테리어 도면 설계</span>
+        </p>
+        <img src="/images/interior3.png" alt="공작 사진" />
       </div>
       <div>
+        <p className={styles.cover}>
+          가구
+          <br />
+          <span>맞춤 가구 제작</span>
+        </p>
         <img src="/images/factory2.png" alt="공작 사진" />
       </div>
       <div>
         <h2>
-         한 업체에서 모든 인테리어를 <br />다 하지 않아도 되요!
+          모든 인테리어를 꼭 한 업체에서
+          <br />
+          진행하지 않아도 됩니다.
         </h2>
         <p>
-          모든 인테리어를 꼭 한 업체에서만 하실 필요는 없어요.
-          <br />
-          업체별 여기 저기 견적을 부분별 확인해보신후, <br /> 보다 저렴하고 합리적인 가격에 인테리어가 가능합니다.
+          {" "}
+          여러 업체에서 견적을 받아보고, <br />
+          부분별로 인테리어를 진행하면
+          <br />더 합리적이고 완성도 높은 결과를 얻을 수 있습니다. <br />
+          맞춤형 접근으로 원하는 공간을 효율적으로 완성해보세요.{" "}
         </p>
         <Link to="/guid">
           인테리어 가이드 보기 <IconRightArrow direction={"right"} />
         </Link>
       </div>
       <div>
-        <img src="/images/factory3.png" alt="공작 사진" />
+        <p className={styles.cover}>
+          인테리어
+          <br />
+          <span>전문 분야별 시공 및 설치</span>
+        </p>
+        <div>
+          <Slider {...settings}>
+            <div className={styles.factory_item}>
+              <img src="/images/interior.png" alt="공작 사진" />
+            </div>
+            <div className={styles.factory_item}>
+              <img src="/images/interior5.png" alt="공작 사진" />
+            </div>
+            <div className={styles.factory_item}>
+              <img src="/images/interior6.png" alt="공작 사진" />
+            </div>
+            
+          </Slider>
+        </div>
       </div>
     </div>
   );
